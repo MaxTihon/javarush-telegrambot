b740552d2ede0fff3e8e5403c673bede1c6d10c2
Max_Tihon <maksim9542@gmail.com>
2021-04-27 11:00:39 +0300

    JRTB-2: added @Component annotation to the JavaRushTelegramBot

--- src/main/java/com/github/javarushcommunity/jrtb/bot/JavaRushTelegramBot.java
+++ src/main/java/com/github/javarushcommunity/jrtb/bot/JavaRushTelegramBot.java
@@ -1,11 +1,13 @@
 package com.github.javarushcommunity.jrtb.bot;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
+@Component
 public class JavaRushTelegramBot extends TelegramLongPollingBot {
 
     @Value("${bot.username}")
